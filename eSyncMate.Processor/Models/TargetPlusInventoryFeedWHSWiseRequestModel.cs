acd2767b690f0b5b8bad7e94839511dd7898ecf9
Muhammad Bilal <33339692+MughalBilal@users.noreply.github.com>
2026-02-23 22:51:26 +0500

    make changes For TargetPlus Inventory Feed

---
+++ eSyncMate.Processor/Models/TargetPlusInventoryFeedWHSWiseRequestModel.cs
@@ -0,0 +1,22 @@
+﻿namespace eSyncMate.Processor.Models
+{
+    public class TargetPlusInventoryFeedWHSWiseRequestModel
+    {
+
+        public List<TargetPlusQuantity> quantities { get; set; }
+
+
+        public TargetPlusInventoryFeedWHSWiseRequestModel() 
+        {
+            this.quantities = new List<TargetPlusQuantity>();
+        }
+
+
+        public class TargetPlusQuantity
+        {
+            public string distribution_center_id { get; set; }
+            public int quantity { get; set; }
+        }
+
+    }
+}
